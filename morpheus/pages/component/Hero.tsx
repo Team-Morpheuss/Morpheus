b327496dd2a76726898ddd6739f0d5d1eb2810c6
UkemeSkywalker <skywalkershadow9@gmail.com>
2022-09-16 16:25:20 +0100

    change gifs

--- morpheus/pages/component/Hero.tsx
+++ morpheus/pages/component/Hero.tsx
@@ -4,16 +4,22 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-    <Box borderRadius="5%" className="bg-white bg-opacity-50 backdrop-blur-xl rounded drop-shadow-lg " p={3}>
-
-          
-          <Box>
-            <Image  w="100%" h="100%" borderRadius="7%" src="./nft/fi.gif" alt="" />
-          </Box>
-          
-       
+      <Box
+        borderRadius="5%"
+        className=""
+        p={3}
+      >
+        <Box>
+          <Image
+            w="100%"
+            h="100%"
+            borderRadius="7%"
+            src="./nft/nft30.gif"
+            alt=""
+          />
         </Box>
-        </>
+      </Box>
+    </>
   )
 }
 
